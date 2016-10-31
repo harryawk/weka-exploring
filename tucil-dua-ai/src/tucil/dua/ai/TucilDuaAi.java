c0ed07740ca63a454bc01a0234fe26cb79f5d4df
AditioPangestu <aditiopangestu@gmail.com>
2016-10-31 14:00:28 +0700

    tambah discretize

--- tucil-dua-ai/src/tucil/dua/ai/TucilDuaAi.java
+++ tucil-dua-ai/src/tucil/dua/ai/TucilDuaAi.java
@@ -7,6 +7,9 @@ package tucil.dua.ai;
 
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.Discretize;
+import weka.filters.unsupervised.attribute.Remove;
 
 /**
  *
@@ -17,20 +20,31 @@ public class TucilDuaAi {
     static Instances datas;
     
     public static void LoadData() throws Exception {
-        datas = DataSource.read("C:\\Program Files\\Weka-3-8\\data\\iris.arff");
-//        System.out.println(datas);
+        datas = DataSource.read
+        ("C:\\Program Files\\Weka-3-8\\data\\iris.arff");
+        datas.setClassIndex(datas.numAttributes()-1); //Set label atribute
     }
-    /**
-     * @param args the command line arguments
-     */
+    
+    public static Instances Discretize() throws Exception {
+        Discretize discretize = new Discretize();
+        String[] options = new String[2];
+        options[0] = "-R";                                    
+        options[1] = "1";                                     
+        discretize.setOptions(options);
+        discretize.setInputFormat(datas);
+        return Filter.useFilter(datas,discretize);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        TucilDuaAi test= new TucilDuaAi();
         try {
             LoadData();
+            Instances temp = Discretize();
+            System.out.println(temp);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println(datas);
     }
     
 }
